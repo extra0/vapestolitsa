a6df1d337ce62ba0f568ac5813b4fec124205586
Ростислав <extra0@mail.ru>
2016-02-24 16:20:06 +0300

    thankyou page done

--- js/script.js
+++ js/script.js
@@ -21,17 +21,33 @@ $(function(){
 	$('.search-form__input').keyup(function(){
 		if ($(this).val().length >= 3) {
 			$(this).addClass('smart-open')
-			$('.smart-result__block').fadeIn(400);
+			$('.smart-result__block').show();
 		} else {
 			$(this).removeClass('smart-open');
-			$('.smart-result__block').fadeOut(400);
+			$('.smart-result__block').hide();
 		}
 	});
 
 	// показываем меню залогиненного пользователя
 	$('.user__link-login').click(function(){
-		$(this).next().toggleClass('active');
+		$('.user__drop-down').toggleClass('active');
 		$(this).toggleClass('active');
+	});
+
+	// сворачиваем меню пользователя по клику вне его области
+	// $(document).mouseup(function(e) {
+	// 	var container = $(".user__drop-down");
+	// 	if (container.has(e.target).length === 0) {
+	// 		$('.user__link-login').removeClass('active');
+	// 		container.removeClass('active');
+	// 	}
+	// });
+
+	// закрываем меню по ESC
+	$(document).keyup(function(d) {
+		if (d.keyCode == 27) {
+			$('.user__drop-down, .user__link-login').removeClass('active');
+		}
 	});
 
 
